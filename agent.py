76f9bb7ee690a5837ae0e43b8f9562926cf73d9e
SenYanHo <80093853+123jsen@users.noreply.github.com>
2022-12-22 16:04:23 +0800

    More template code for agent

--- agent.py
+++ agent.py
@@ -1,21 +1,55 @@
+import numpy as np
+import random
+
 from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+# 1440 * 5 for stock price, 1 for current BTC, 1 for current BUSD
+STATE_SIZE = 1440 * 5 + 2
+ACTION_SIZE = 7
 
 class Agent:
-    def __init__(self, gamma=0.95, lr=0.001):
+    def __init__(self, gamma=0.95, lr=0.001, epsilonDecay=0.995, epsilonMin=0.01):
+        # Hyperparameters
         self.gamma = gamma
         self.lr = lr
+        self.epsilon = 1.0
+        self.epsilonDecay = epsilonDecay
+        self.epsilonMin = epsilonMin
+
+        # Other structures
         self.memory = deque(maxlen=2000)
-        self.model = self._build_model()
+        self.model = self._buildModel()
 
-    # Model will be LSTM/CNN
-    def _build_model(self):
-        pass
+    # DQN using simple FCNN
+    def _buildModel(self):
+        model = Sequential()
+        model.add(Dense(64, activation='relu', input_dim=STATE_SIZE))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(ACTION_SIZE, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.lr))
+        return model
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+    def remember(self, state, action, nextState, done, reward):
+        self.memory.append((state, action, nextState, done, reward))
 
     def train(self, batch_size):
-        pass
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, nextState, done, reward in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(nextState)[0]))
+                targetF = self.model.predict(state)
+                targetF[0][action] = target
+                self.model.fit(state, targetF, epochs=1, verbose=0)
+            if self.opsilon > self.epsilonMin:
+                self.epsilon *= self.epsilon_decay
 
     def act(self, state):
-        pass
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(ACTION_SIZE)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
