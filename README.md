f00924d472375fc39617b1272e82c5125b8c85bf
SenYanHo <80093853+123jsen@users.noreply.github.com>
2022-12-22 15:40:49 +0800

    Environment completed

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ This python module acts as an environment in which you can plug in a reinforceme
 ## Specifications
 `environment.py` contains a class `CryptoEnv`, which simulates the environment of a MDP.
 
-- **State**: Contains an array of size `5 x 1440`, which is an array of 1440 minutes (24 hours) of `(open, high, low, close, volume)` data, then contains `bitcoinAmount` and `cashAmount`. (1440 is a adjustable parameter)
+- **State**: A tuple containing an array of size `5 x 1440`, which is an array of 1440 minutes (24 hours) of `(open, high, low, close, volume)` data, then contains `BTCamount` and `BUSDamount`. (1440 is a adjustable parameter)
 
-- **Reward**: Returns the gain/loss by the player at this timestep, i.e. `bitcoinAmount * todayClose - bitcoinAmount * prevClose`
+- **Reward**: Returns the gain/loss by the player at this timestep, i.e. `BTCamount * todayClose - BTCamount * prevClose`
 
 - **Action**: At this moment, the player can input a number to denote the amount of bitcoin that the agent wants to buy/sell.
 
-Training data is obtained from binance's public API
+Every episode of the simulation consists of 1440 timesteps, which loads the most recent 1440 entries of the price data. This means two days of price data is loaded for every day of simulation.
+
+## Agent
+
+The demo agent will be DQN based, and it can take the discrete actions: `+100/+50/+10/0/-10/-50/-100`, which buys BTC using `x` amount of BUSD
+
+Training data is price data of BTC to BUSD trading pair, obtained from binance's public API.
--- environment.py
+++ environment.py
@@ -1,24 +1,79 @@
 import numpy as np
+from datetime import date, timedelta
+
+EPISODE_LEN = 1440
 
 
 class CryptoEnv:
-    """Acts as the environment for RL, provides step function that returns next state and reward"""
-    def __init__(self, ticker_symbol, datetime):
-        time = 0
+    def __init__(self):
+        self.time = 0   # time step (in minutes) for simulation
+        self.BTC = 0
+        self.BUSD = 1000
 
-        pass
+        self.data = np.empty(0)
 
-    def reset():
-        pass
+    def reset(self, targetDate):
+        self.time = 0
+        self.BTC = 0
+        self.BUSD = 1000
 
-    def step(action):
-        pass
+        delta = timedelta(days=1)
+        self.data = np.concatenate((CryptoEnv.loadByDate(targetDate - delta),
+                                   CryptoEnv.loadByDate(targetDate)))
+
+        priceState = self.data[self.time:EPISODE_LEN+self.time]
+        return (priceState, self.BTC, self.BUSD)
+
+    def step(self, BUSD):
+        """Buy/Sell BTC at new timestep with BUSD amount (calculate with closing price), Returns nextState, done, reward, totalValue"""
+
+        # Previous values
+        prevTotalValue = self.BUSD + self.BTC * \
+            self.data[EPISODE_LEN+self.time-1][3]
+
+        # Progress time
+        self.time += 1
+        priceState = self.data[self.time:EPISODE_LEN+self.time]
+
+        # Update values
+        if (BUSD > self.BUSD):   # insufficient money for purchase
+            BUSD = self.BUSD
+
+        # amount of BTC purchase, can be +/-
+        purchaseAmount = BUSD / priceState[-1][3]
+        if (purchaseAmount < -self.BTC):  # purchase sold less than total possible
+            purchaseAmount = self.BTC
+
+        self.BUSD -= BUSD
+        self.BTC += purchaseAmount
+
+        newTotalValue = self.BUSD + self.BTC * priceState[-1][3]
+        reward = newTotalValue - prevTotalValue
+
+        # Check done
+        if (self.time == EPISODE_LEN):
+            done = 1
+        else:
+            done = 0
+
+        return ((priceState, self.BTC, self.BUSD), done, reward, newTotalValue)
 
     @staticmethod
-    def load(pathname):
+    def loadByDate(targetDate: date):
+        # Change this path name
+        data = CryptoEnv.load(
+            f"./training-data/BTCBUSD-1m-{targetDate.year}-{targetDate.month}/BTCBUSD-1m-{targetDate.year}-{targetDate.month}.csv")
+        startIndex = (targetDate.day - 1) * 60 * 24
+        endIndex = targetDate.day * 60 * 24
+
+        assert(endIndex < len(data) + 1)  # end index is inside
+
+        return data[startIndex:endIndex]
+
+    @staticmethod
+    def load(pathname: str):
         """Load from .csv assuming format of binance K-line data"""
         data = np.genfromtxt(pathname, delimiter=',')
 
         # The columns are [Open High Low Close Volume]
-        return data[:,1:6]
-
+        return data[:, 1:6]
